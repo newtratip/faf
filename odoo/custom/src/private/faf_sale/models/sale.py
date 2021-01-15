4c68af79088ae1370dc1de2d81c92cd678318dc1
newtratip <tharathip.chaweewongphan@gmail.com>
2021-01-15 13:28:31 +0700

    [14.0][ADD] faf_sale

---
+++ odoo/custom/src/private/faf_sale/models/sale.py
@@ -0,0 +1,31 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    # New fields
+    relocation = fields.Text(
+        string="Relocation",
+    )
+    mode_shipment = fields.Text(
+        string="Mode of Shipment",
+    )
+    not_exceeding_estimated_volume = fields.Text(
+        string="Not exceeding the estimated volume of",
+    )
+    first_day_packing = fields.Char(
+        string="First Day of Packing",
+    )
+    last_day_packing = fields.Char(
+        string="Last Day of Packing",
+    )
+    transit_time = fields.Char(
+        string="Port to Port Transit Time",
+    )
+    customs_clearlance = fields.Char(
+        string="Customs Clearlance",
+    )
