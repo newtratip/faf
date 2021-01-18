f40065c893e1e5e8cd1267f597a7685a82b77883
newtratip <tharathip.chaweewongphan@gmail.com>
2021-01-19 01:36:54 +0700

    [14.0][ADD] faf_forms

---
+++ odoo/custom/src/private/faf_forms/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "FAF - Forms",
+    "summary": "FAF Forms",
+    "version": "14.0.1.0.0",
+    "category": "FAF",
+    "website": "http://ecosoft.co.th",
+    "author": "Tharathip C., Ecosoft",
+    "depends": ["faf_sale", "faf_sale_service_template", "faf_hr_salesperson"],
+    "data": [
+        "data/paperformat_data.xml",
+        "data/report_data.xml",
+        "report/quotation_form.xml",
+        "views/assets.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "maintainers": ["newtratip"],
+}
