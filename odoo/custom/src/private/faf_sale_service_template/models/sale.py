ccb46ab02e08b695a1f10f10a5780dae49334fa2
newtratip <tharathip.chaweewongphan@gmail.com>
2021-01-19 18:51:20 +0700

    [14.0][ENH] Add service include & exclude tab on sale order

--- odoo/custom/src/private/faf_sale_service_template/models/sale.py
+++ odoo/custom/src/private/faf_sale_service_template/models/sale.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -13,3 +13,14 @@ class SaleOrder(models.Model):
         index=True,
         ondelete="restrict",
     )
+    service_include_description = fields.Html()
+    service_exclude_description = fields.Html()
+
+    @api.onchange("service_template_id")
+    def _onchange_service_template_id(self):
+        self.service_include_description = (
+            self.service_template_id.service_include_description
+        )
+        self.service_exclude_description = (
+            self.service_template_id.service_exclude_description
+        )
