8470aa4cac72ae9c05819801ec95e0cf355183a8
newtratip <tharathip.chaweewongphan@gmail.com>
2021-01-24 16:21:44 +0700

    [14.0][UPD] Add sale_order_type module

--- odoo/custom/src/private/faf_install/__manifest__.py
+++ odoo/custom/src/private/faf_install/__manifest__.py
@@ -24,6 +24,7 @@
         "project_list",
         "project_status",
         "project_deadline",
+        "sale_order_type",
         # FAF Modules
         "faf_hr_salesperson",
         "faf_sale",
